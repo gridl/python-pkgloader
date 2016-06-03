c4b4c266e47760b8828cdfa36c5d58dced30e331
Marko Kreen <markokr@gmail.com>
2016-06-03 14:00:53 +0300

    Move pkgloader to separate repo

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "pkgloader",
+    license = "ISC",
+    version = '1.0',
+    maintainer = "Marko Kreen",
+    maintainer_email = "markokr@gmail.com",
+    py_modules = ['pkgloader'],
+)
+
